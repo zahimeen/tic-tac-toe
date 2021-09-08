31fe335decbe71b98e899d1bc0912b50633d85fc
zahin-muhaimeen <zahin.muhaimeen@gmail.com>
2021-09-08 19:49:41 +1000

    check win vertically and fix some errors

--- src/script.js
+++ src/script.js
@@ -6,6 +6,56 @@ let currentPlayer, disabledButtons, turnsTaken, board;
 
 const setMessage = (msg) => (messageEl.textContent = msg);
 
+const isThreeInARow = function (desiredValue, values) {
+    for (const val of values) {
+        if (val !== desiredValue) return false;
+    }
+    return true;
+};
+
+const isVerticalWin = function (playerVal, posX) {
+    const vals = [];
+    for (let posY = 0; posY < 3; posY++) {
+        const val = board[posY][posX];
+        vals.push(val);
+    }
+    return isThreeInARow(playerVal, vals);
+};
+
+const getGameStatus = function (playerVal, posX, posY) {
+    if (turnsTaken < 5) return { msg: "", isFinished: false };
+
+    if (isVerticalWin(playerVal, posX))
+        return { msg: `Player ${playerVal} Wins!`, isFinished: true };
+
+    if (turnsTaken >= 9) return { msg: "Draw!", isFinished: true };
+    return { msg: "", isFinished: false };
+};
+
+const setGameStatus = function (newPlayer, posX, posY) {
+    const gameStatus = getGameStatus(players[currentPlayer], posX, posY);
+    if (gameStatus.isFinished) {
+        disableButtons();
+        setMessage(gameStatus.msg);
+        return;
+    }
+    setMessage(`It is Player ${players[newPlayer]}'s Turn`);
+};
+
+const setPosition = function (posEl, posX, posY) {
+    const isBlankPos = board[posY][posX] === "";
+    if (isBlankPos) {
+        const newPlayer = currentPlayer ? 0 : 1;
+        board[posY][posX] = players[currentPlayer];
+        posEl.textContent = players[currentPlayer];
+        turnsTaken += 1;
+        setGameStatus(newPlayer, posX, posY);
+        currentPlayer = newPlayer;
+        return;
+    }
+    setMessage("This position is occupied!");
+};
+
 const positionSelected = function () {
     const [strPosY, strPosX] = this.id.split(",");
     const [posX, posY] = [Number(strPosX), Number(strPosY)];
@@ -17,35 +67,6 @@ const disableButtons = function () {
         pos.removeEventListener("click", positionSelected);
     }
     disabledButtons = true;
-};
-
-const getGameStatus = function () {
-    if (turnsTaken < 5) return { msg: "", isFinished: false };
-    if (turnsTaken >= 9) return { msg: "Draw!", isFinished: true };
-};
-
-const setGameStatus = function (newPlayer) {
-    const gameStatus = getGameStatus();
-    if (gameStatus.isFinished) {
-        disableButtons();
-        setMessage(gameStatus.msg);
-        return;
-    }
-    setMessage(`It is Player ${players[newPlayer]}'s Turn`);
-};
-
-const setPosition = function (posEl, posX, posY) {
-    const isBlankPos = board[posX][posY] === "";
-    if (isBlankPos) {
-        const newPlayer = currentPlayer ? 0 : 1;
-        board[posX][posY] = players[currentPlayer];
-        posEl.textContent = players[currentPlayer];
-        currentPlayer = newPlayer;
-        turnsTaken += 1;
-        setGameStatus(newPlayer);
-        return;
-    }
-    setMessage("This position is occupied!");
 };
 
 const newGame = function () {
