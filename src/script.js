12a06ea81598cafc61253f5d9e856437b274c304
zahin-muhaimeen <zahin.muhaimeen@gmail.com>
2021-10-06 20:38:58 +1000

    how am i supposed to describe a bad refactor

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const containerBoard = document.querySelector(".board");
 const btnNewGame = document.querySelector(".new-game__btn");
 const labelMessage = document.querySelector(".message__label");
@@ -6,10 +8,13 @@ const valueScores = [...document.querySelectorAll(".score__value")];
 const playerValues = ["X", "O"];
 const boardLength = 3;
 const gameWinningLength = 3;
+const minimumPlayCount = gameWinningLength * 2 - 1;
+const drawPlayCount = boardLength ** 2;
 
-let guiBoard, localBoard, currentPlayer, scores, playing;
+let guiBoard, localBoard, currentPlayer, playing, playCount;
+let scores = [0, 0];
 
-const createBoard = function (length = 3) {
+const createBoard = function (length) {
     containerBoard.replaceChildren();
 
     const style = `
@@ -21,48 +26,27 @@ const createBoard = function (length = 3) {
         width: calc(var(--board-length) / ${length});
         font-size: calc(var(--board-min-length) / ${length + 0.5});
     `;
-    const board = [];
+    localBoard = [];
+    guiBoard = [];
 
     for (let y = 0; y < length; y++) {
-        board.push([]);
+        localBoard.push([]);
+        guiBoard.push([]);
         for (let x = 0; x < length; x++) {
-            board[y].push("");
+            localBoard[y].push("");
 
-            containerBoard.insertAdjacentHTML(
-                "beforeend",
-                `
+            const btnHTML = `
                 <button 
                     type="button" 
                     class="board__btn pos--x-${x} pos--y-${y}"
                     style="${style}" 
                 ></button>
-                `,
-            );
+            `;
+            containerBoard.insertAdjacentHTML("beforeend", btnHTML);
         }
     }
 
-    localBoard = board;
     guiBoard = [...document.querySelectorAll(".board__btn")];
-};
-
-const updateMessage = (msg) => (labelMessage.textContent = msg);
-
-const updateScores = function (player) {
-    if (Number.isFinite(player)) scores[player]++;
-    valueScores.forEach((score, i) => (score.textContent = scores[i]));
-};
-
-const updateBoard = function (btn, xCoords, yCoords) {
-    if (localBoard[yCoords][xCoords] === "") {
-        const value = playerValues[currentPlayer];
-        localBoard[yCoords][xCoords] = value;
-        btn.textContent = value;
-
-        currentPlayer = currentPlayer ? 0 : 1;
-        updateMessage(`it is ${playerValues[currentPlayer]}'s turn!`);
-        return;
-    }
-    updateMessage(`that position is occupied!`);
 };
 
 const toggleBoardFunctionality = function () {
@@ -81,10 +65,111 @@ const toggleBoardFunctionality = function () {
     guiBoard.forEach((btn) => (btn.disabled = true));
 };
 
+const highlightBtns = function (coords) {
+    coords.forEach(([x, y]) =>
+        document
+            .querySelector(`.pos--x-${x}.pos--y-${y}`)
+            .classList.add("board__btn--highlight"),
+    );
+};
+
+const isWinning = function (vals) {
+    let highConsec = 0;
+    let currConsec = 0;
+    for (const val of vals) {
+        if (val !== playerValues[currentPlayer]) {
+            if (currConsec > highConsec) highConsec = currConsec;
+            currConsec = 0;
+            continue;
+        }
+        currConsec++;
+    }
+    if (currConsec > highConsec) highConsec = currConsec;
+    return highConsec >= gameWinningLength;
+};
+
+const getValues = function (coordinates) {
+    const values = [];
+    coordinates.forEach(([x, y]) => values.push(localBoard[y][x]));
+    return values;
+};
+
+const getWinningCoords = function (xCoords, yCoords) {
+    const coords = [[], [], [], []];
+    const [verCoords, horCoords, posDiaCoords, negDiaCoords] = coords;
+    for (let i = 0; i < 3; i++) {
+        verCoords.push([xCoords, i]);
+        horCoords.push([i, yCoords]);
+        posDiaCoords.push([i, gameWinningLength - 1 - i]);
+        negDiaCoords.push([i, i]);
+    }
+
+    const vals = coords.map((cds) => getValues(cds));
+    for (const [i, val] of vals.entries()) {
+        const isWin = isWinning(val);
+        if (isWin) return coords[i];
+    }
+};
+
+const getGameStatus = function (xCoords, yCoords) {
+    if (playCount >= drawPlayCount) {
+        for (const btn of guiBoard) {
+            btn.classList.add("board__btn--highlight");
+        }
+        return { msg: "Draw!", playing: false };
+    }
+
+    if (playCount >= minimumPlayCount) {
+        const winningCoords = getWinningCoords(
+            Number.parseInt(xCoords),
+            Number.parseInt(yCoords),
+        );
+        if (winningCoords) {
+            updateScores(currentPlayer);
+            highlightBtns(winningCoords);
+            return {
+                msg: `${playerValues[currentPlayer]} won!`,
+                playing: false,
+            };
+        }
+    }
+
+    return {
+        msg: `it is ${playerValues[currentPlayer]}'s turn!`,
+        playing: true,
+    };
+};
+
+const updateMessage = (msg) => (labelMessage.textContent = msg);
+
+const updateScores = function (player) {
+    if (Number.isFinite(player)) scores[player]++;
+    valueScores.forEach((score, i) => (score.textContent = scores[i]));
+};
+
+const updateBoard = function (btn, xCoords, yCoords) {
+    if (localBoard[yCoords][xCoords] !== "")
+        return updateMessage(`that position is occupied!`);
+
+    const value = playerValues[currentPlayer];
+    localBoard[yCoords][xCoords] = value;
+    btn.textContent = value;
+    playCount++;
+
+    const gameStatus = getGameStatus(xCoords, yCoords);
+    if (!gameStatus.playing) {
+        playing = false;
+        toggleBoardFunctionality();
+    }
+    updateMessage(gameStatus.msg);
+
+    currentPlayer = currentPlayer ? 0 : 1;
+};
+
 const createGame = function () {
     playing = true;
+    playCount = 0;
     currentPlayer = 0;
-    scores = [0, 0];
 
     createBoard(boardLength);
     updateScores();
